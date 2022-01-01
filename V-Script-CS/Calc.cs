baa077ba900472b348bab25a6634a8345404660e
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-01 11:28:50 +0800

    Added "Math" class

--- V-Script-CS/Calc.cs
+++ V-Script-CS/Calc.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,6 +43,40 @@ namespace V_Script_CS
                     Console.WriteLine(a / b);
                     break;
             }
+        }
+    }
+
+    class Math
+    {
+        public int res;
+        public int a;
+        public int b;
+
+        public void InitNums()
+        {
+            Console.Write("Input your first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input your second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Add()
+        {
+            res = a + b;
+        }
+
+        public void Sub()
+        {
+            res = a - b;
+        }
+
+        public void Multiply()
+        {
+            res = a * b;
+        }
+
+        public void Divide()
+        {
+            res = a / b;
         }
     }
 }
