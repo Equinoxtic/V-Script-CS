71d71772269aad633dc68724a9a07a421df3735f
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-04 22:19:48 +0800

    namespace change update thing idk

---
+++ V-Script-CS/States/LoopSelectionState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V_Script_CS.States
+{
+    class LoopSelectionState
+    {
+        public void LoopSelector()
+        {
+            string curSelected;
+            Utility.Loops LocalLoops = new Utility.Loops();
+            Console.WriteLine("Select a loop: ");
+            Console.Write("[vF] For Loop | [vW] While Loop\n");
+            curSelected = Console.ReadLine();
+
+            switch (curSelected)
+            {
+                case "vF":
+                    LocalLoops.VFor();
+                    break;
+
+                case "vW":
+                    LocalLoops.VWhile();
+                    break;
+            }
+        }
+    }
+}
