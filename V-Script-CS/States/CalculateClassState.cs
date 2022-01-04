f4a9d7c721460a6708cd1dcae8112b78e9be8de9
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-04 16:03:31 +0800

    update to 1.1

--- V-Script-CS/States/CalculateClassState.cs
+++ V-Script-CS/States/CalculateClassState.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,12 +12,12 @@ namespace V_Script_CS.States
         {
             int curSelected;
 
-            Calc Calculator = new Calc();
+            Calculate Calculator = new Calculate();
             Math VMath = new Math();
             States.MathSelectionState MathSelectState = new States.MathSelectionState();
 
             Console.WriteLine("Select a function / class: ");
-            Console.Write("[1] Open Calculator (Function)\n[2] Math (Class)\n");
+            Console.Write("[1] Open Calculator (Function) | [2] Math (Class)\n");
             curSelected = Convert.ToInt32(Console.ReadLine());
 
             switch (curSelected)
