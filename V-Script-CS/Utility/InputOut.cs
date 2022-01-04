81b517c1290614160e7a106315fd526f47b837fa
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-04 16:23:26 +0800

    add more functions

--- V-Script-CS/Utility/InputOut.cs
+++ V-Script-CS/Utility/InputOut.cs
@@ -10,9 +10,23 @@ namespace V_Script_CS.Utility
     {
         public void InputStr(string str)
         {
-            Console.Write("Input some string: ");
+            Console.Write("Input some text for your string: ");
             str = Console.ReadLine();
             Console.WriteLine("String: " + str);
+        }
+
+        public void InputInt(int num)
+        {
+            Console.Write("Input a number for your integer: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Integer: " + num);
+        }
+
+        public void InputFloat(float num)
+        {
+            Console.Write("Input some numbers for your float: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Float: " + num);
         }
     }
 }
