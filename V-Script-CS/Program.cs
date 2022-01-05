b53920f2194e87081ce8add635b69d67a4b2176d
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-05 18:00:02 +0800

    Update to 1.3

--- V-Script-CS/Program.cs
+++ V-Script-CS/Program.cs
@@ -11,14 +11,16 @@ namespace V_Script_CS
         static void Main(string[] args)
         {
             int actionType;
+
             States.CalculateClassState CalculateState = new States.CalculateClassState();
             States.LoopSelectionState SelectLoopState = new States.LoopSelectionState();
             States.CreatorClassState CreateState = new States.CreatorClassState();
             States.InputOutSelector IOState = new States.InputOutSelector();
+            States.LocalObjsSelectState LObjsSelect = new States.LocalObjsSelectState();
 
             Console.WriteLine("===== V-Script ======\n");
             Console.WriteLine("Select an action: ");
-            Console.Write("[1] Calculate | [2] Loops | [3] Object Creator | [4] Basic Input\n");
+            Console.Write("[1] Calculate | [2] Loops | [3] Object Creator | [4] Basic Input | [5] Object Selection\n");
             actionType = Convert.ToInt32(Console.ReadLine());
 
             switch (actionType)
@@ -37,6 +39,10 @@ namespace V_Script_CS
 
                 case 4:
                     IOState.IOSelect();
+                    break;
+
+                case 5:
+                    LObjsSelect.LObjsSelection();
                     break;
             }
         }
